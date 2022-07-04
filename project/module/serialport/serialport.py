9f39d19d9732856965393c35e27e75ec8bbc4c0d
Sahil <sahil.khanna@nutromics.com.au>
2022-07-04 17:18:13 +1000

    added context handler to serial port

--- project/module/serialport/serialport.py
+++ project/module/serialport/serialport.py
@@ -8,9 +8,10 @@ class SerialPort:
         'parity':'N',
         'stopbits':1,
         'timeout':None,
-        'xonxoff':0,
-        'rtscts':0
+        'xonxoff':False,
+        'rtscts':False
     }
+    _sp:Serial
     def __init__(self, comPortName:str = None, conf:dict = None) -> None:
         availablePorts = self.list_serial_ports()
         if comPortName in availablePorts:
@@ -23,6 +24,23 @@ class SerialPort:
                     self._conf[setting]=conf[setting]
                 else:
                     raise ValueError(f'Unsupported or invalid setting {setting}')
+    
+    def __enter__(self):
+        self._sp = Serial()
+        self._sp.port=self._selectedPortName
+        self._sp.baudrate = self._conf['baudrate']
+        self._sp.bytesize=self._conf['bytesize']
+        self._sp.parity=self._conf['parity']
+        self._sp.stopbits=self._conf['stopbits']
+        self._sp.timeout=self._conf['timeout']
+        self._sp.xonxoff=self._conf['xonxoff']
+        self._sp.rtscts=self._conf['rtscts']
+        self.connection = self._sp.open()
+        return self.connection
+    
+    def __exit__(self, exc_type, exc_value, exc_tb):
+        self._sp.close()
+        print(f'Context ended for {self._selectedPortName}')
         
         
     @staticmethod
@@ -41,3 +59,9 @@ if __name__ == "__main__":
     for comport in ports:
         sp = SerialPort(comport,portSetting)
         print(sp._selectedPortName,sp._conf)
+        try:
+            with sp as serialport:
+                print(serialport)
+        except:
+            pass
+                
