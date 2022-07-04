21f0fdbf144b71139efe783308b41a7242b419dc
Sahil <sahil.khanna@nutromics.com.au>
2022-07-04 16:21:47 +1000

    Can List serial ports

--- project/module/serialport/serialport.py
+++ project/module/serialport/serialport.py
@@ -1,3 +1,26 @@
-import serial
+from serial import Serial
+from serial.tools import list_ports
 class SerialPort:
-    pass
+    _selectedPortName:str
+    def __init__(self, comPortName:str = None) -> None:
+        availablePorts = self.list_serial_ports()
+        if comPortName in availablePorts:
+            self._selectedPortName = comPortName
+        else:
+            raise ValueError('Provided port doesn\'t exist')
+        
+    @staticmethod
+    def list_serial_ports() -> dict:
+        ports = list_ports.comports()
+        portList = dict()
+        for port, description, hwid in ports:
+            portList.update({port : description})
+        return portList
+    
+
+if __name__ == "__main__":
+    
+    ports = SerialPort.list_serial_ports()
+    for comport in ports:
+        sp = SerialPort(comport)
+        print(sp._selectedPortName)
