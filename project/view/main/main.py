b6b570f4d9bbbda15f7cbaf968e1e12d76cfe490
Sahil Khanna <mail@sahilkhanna.org>
2022-07-09 16:15:28 +1000

    Updated layout

--- project/view/main/main.py
+++ project/view/main/main.py
@@ -1,14 +1,31 @@
 import PySimpleGUI as gui
 class MainUI:
+    KEY_CONSOLE = "CONSOLE"
+    KEY_MSG_LIST = "MSG_LIST"
+    KEY_PORT_LIST = "PORT_LIST"
+    KEY_ADD_MSG_BTN = "ADD_MSG_BTN"
+    KEY_SEND_MSG_BTN = "SEND_MSG_BTN"
+    KEY_REFRESH_PORT_LIST = "REFRESH_PORT_LIST"
+    KEY_OPEN_PORT = "OPEN_PORT"
+    LEFT_COLUMN_WIDTH = 40
+    RIGHT_COLUMN_WIDTH = 80
     def __init__(self, title:str) -> None:
         # First the window layout in 2 columns
+        gui.theme('DarkBlue')
         conf_column = [
-            [gui.Listbox(values=[], enable_events=True, size=(40, 20), key="-FILE LIST-")],
+            [gui.Text("Console:")],
+                [gui.Combo(values="", default_value="Select Port", size=self.LEFT_COLUMN_WIDTH-20, key=self.KEY_PORT_LIST),
+                 gui.Button(button_text='Refresh', key=self.KEY_REFRESH_PORT_LIST),
+                 gui.Button(button_text='Open', key=self.KEY_OPEN_PORT)],
+                [gui.HSeparator()],
+            [gui.Button(button_text='Add Message', key=self.KEY_ADD_MSG_BTN),
+             gui.Button(button_text='Send', key=self.KEY_SEND_MSG_BTN)],
+            [gui.Listbox(values=[], enable_events=True, size=(self.LEFT_COLUMN_WIDTH, 20), background_color='#0a1016', key=self.KEY_MSG_LIST)],
         ]
         # For now will only show the name of the file that was chosen
         console_column = [
             [gui.Text("Console:")],
-            [gui.Output(size=(80, 20), background_color='black', text_color='green')],
+            [gui.Output(size=(self.RIGHT_COLUMN_WIDTH, 22), background_color='#0a1016', text_color='green', key=self.KEY_CONSOLE)],
         ]
         # ----- Full layout -----
         self._layout = [
@@ -20,12 +37,16 @@ class MainUI:
     pass
 
     def launch(self):
+        console_text=""
         while True:
             event, values = self._ui.read()
+            print(f'event = {event}, values = {values}')
+            if event == gui.WIN_CLOSED:
+                break
+            console_text += "hey\n"
+            self._ui[self.KEY_CONSOLE].update(value=console_text)
             # End program if user closes window or
             # presses the OK button
-            if event == self._ui.WIN_CLOSED:
-                break
 
         # Finish up by removing from the screen
         self._ui.close()
